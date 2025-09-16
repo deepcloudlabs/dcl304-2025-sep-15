0de79cb872b287bc2661192fd53e98e5775592a7
Binnur KURT <deepcloudlabs@gmail.com>
2025-09-16 10:55:45 +0300

    Update exercise08.js
    refactor and remove the warnings

--- module03-functions/exercise08.js
+++ module03-functions/exercise08.js
@@ -1,17 +1,19 @@
-let sum_left = 0;
-let sum_right = 0;
-let i=0;
-
-function get_sum_of_building_numbers(begin_no,end_no,limit) {
-    let sum = 0;
-    for (i = begin_no; i < end_no && sum < limit; sum += i,i++) ;
-    return [sum, i-1];
+function get_sum_of_building_numbers(begin_no, end_no, limit) {
+    let sum = 0, i;
+    for (i = begin_no; i < end_no && sum < limit; i++) {
+        sum += i;
+    }
+    return [sum, i - 1];
 }
 
-for (let N=2; N<1_000_000; N+=1){
-    let [sum_left,left_index] = get_sum_of_building_numbers(0,N,N**2);
-    let [sum_right,M] = get_sum_of_building_numbers(N+1,N**2,sum_left)
-    if (sum_right === sum_left){
-        console.log(`N=${N}, M=${M}`);
-    }
+function test_building_no(N) {
+    let [sum_left] = get_sum_of_building_numbers(0, N, N ** 2);
+    let [sum_right, M] = get_sum_of_building_numbers(N + 1, N ** 2, sum_left)
+    return [sum_right === sum_left, M]
+}
+
+for (let N = 2; N < 100_000; N += 1) {
+    let [lives_at_the_building, M] = test_building_no(N);
+    if (lives_at_the_building)
+        console.log(`The mathematician lives at the building #${N}. There are ${M} buildings in the street!`);
 }
