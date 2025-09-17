e9641243163f7b48c216426d1b13dc5c134a0309
Binnur KURT <deepcloudlabs@gmail.com>
2025-09-17 09:57:35 +0300

    Create exercise01.js

---
+++ module04-oop/oop-loop/exercise01.js
@@ -0,0 +1,16 @@
+let jack = {
+    first_name: 'Jack',
+    last_name: 'Bauer',
+    identity: 1234567890,
+    salary: 1000000,
+    iban: "TR1",
+    email: "jack.bauer@mail.com",
+    birth_year: 1968,
+    greet: function() {
+        console.log(`Hello, ${this.first_name}, ${this.last_name}!`);
+    }
+}
+
+for (let key in jack){ // reflection
+    console.log(`jack[${key}]: ${jack[key]}`);
+}
