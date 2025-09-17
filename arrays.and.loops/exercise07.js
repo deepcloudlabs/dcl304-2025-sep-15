74b21f647cae1ee7a81fecc6a514b9980a11e3a9
Binnur KURT <deepcloudlabs@gmail.com>
2025-09-17 09:57:33 +0300

    Update exercise07.js

--- arrays.and.loops/exercise07.js
+++ arrays.and.loops/exercise07.js
@@ -2,16 +2,32 @@ let numbers = new Array(4, 8, 15, 16, 23, 42);
 console.log(numbers[0]);
 console.log(numbers[1]);
 console.log(numbers[2]);
-console.log(numbers.length)
+console.log(numbers.length) // 6
 numbers[100] = 100;
-console.log(numbers.length)
+console.log(numbers.length) // 101
 console.log(numbers[99]);
 numbers[-20] = 20;
-console.log(numbers.length)
-console.log(numbers[-20]);
-console.log(numbers["-20"]);
-console.log(numbers["100"]);
+console.log(numbers.length) // 101
+console.log(numbers[-20]); // 20
+console.log(numbers["-20"]); // 20
+console.log(numbers["100"]); // 100
 numbers[3.1415] = "jack bauer";
-console.log(numbers[3.1415]);
-console.log(numbers.length)
-console.log(typeof(numbers))
+console.log(numbers[3.1415]); // jack bauer
+console.log(numbers.length) // 101
+console.log(numbers)
+x = {}
+x[3.1415] = "pi";
+console.log(x)
+for (let i = 0; i < numbers.length; i++) { // array
+    console.log(numbers[i]);
+}
+
+for (let i in numbers) { // object + array
+    console.log(i,numbers[i]);
+}
+
+for (let number of numbers) { // array
+    console.log(number);
+}
+
+numbers.forEach(function(number) {console.log(number);}); // array
