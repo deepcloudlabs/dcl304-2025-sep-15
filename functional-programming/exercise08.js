bdb00109d8addb07696aecce9bcc15f27b06b5a7
Binnur KURT <deepcloudlabs@gmail.com>
2025-09-17 11:57:46 +0300

    Update exercise08.js

--- functional-programming/exercise08.js
+++ functional-programming/exercise08.js
@@ -1,4 +1,4 @@
-const cuntries = require("./resources/countries.json");
+const movies = require("./resources/movies.json");
 // movies with multiple directors
 const if_movie_with_multiple_directors = movie => movie.directors.length > 1;
 const movie_print_imdb_url = movie => console.log(`https://www.imdb.com/title/${movie.imdb}`);
